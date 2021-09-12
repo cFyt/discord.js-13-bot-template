df173430899a6861935b670f1378d81a486b9794
cFyt <45104771+cFyt@users.noreply.github.com>
2021-09-12 18:30:46 +0300

    Update commandRefresh.js

--- utils/commandRefresh.js
+++ utils/commandRefresh.js
@@ -4,16 +4,18 @@ const config = require('../config.json')
 const rest = new REST({ version: '9' }).setToken(config.token);
 
 function commandRefresh(guildlID, commands){
+  
     (async () => {
+      
         try {
-          console.log('Adding commands to guild...');
+          console.log('Started refreshing application (/) commands.');
       
           await rest.put(
             Routes.applicationGuildCommands(config.appID, guildlID),
-            { body: commands },
+            { body: commands, },
           );
       
-          console.log('Commands added successfully.');
+          console.log('Successfully reloaded application (/) commands.');
         } catch (error) {
           console.error(error);
         }
@@ -21,4 +23,4 @@ function commandRefresh(guildlID, commands){
 }
 module.exports = {
   commandRefresh
-} 
+}
