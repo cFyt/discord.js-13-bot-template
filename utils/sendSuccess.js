ed943cb17bc465e799c0c0cd766713dc31b85d6b
cFyt <45104771+cFyt@users.noreply.github.com>
2021-09-12 18:32:36 +0300

    Update sendSuccess.js

--- utils/sendSuccess.js
+++ utils/sendSuccess.js
@@ -1,15 +1,31 @@
-const { MessageEmbed ,Message } = require('discord.js')
+const { MessageEmbed, Message } = require('discord.js')
 /**
  * @param {Message} message
  * @param {String} msg
  */
-function sendSuccess(message, msg) {
-    let errembed = new MessageEmbed()
-    .setAuthor(message.author.username,message.member.user.displayAvatarURL())
-    .setColor("DARK_GREEN")
-    .setTimestamp()
-    .setDescription(msg)
-    message.channel.send({embeds: [errembed]})
+function sendSuccess(message, msg, reply) {
+    if(reply){
+        message.channel.send({
+            embeds: [
+                new MessageEmbed()
+                    .setColor("DARK_GREEN")
+                    .setAuthor(message.member.user.username, message.member.user.displayAvatarURL())
+                    .setTimestamp()
+                    .setDescription(msg)
+            ]
+        })
+    }else {
+        message.reply({
+            embeds: [
+                new MessageEmbed()
+                    .setColor("DARK_GREEN")
+                    .setAuthor(message.member.user.username, message.member.user.displayAvatarURL())
+                    .setTimestamp()
+                    .setDescription(msg)
+            ]
+        })
+    }
+    
 }
 module.exports = {
     sendSuccess
