54e0003c4ed4b75880d2369bdbc66819648f7220
cFyt <45104771+cFyt@users.noreply.github.com>
2021-09-12 18:34:41 +0300

    Update ping.js

--- slashCommands/info/ping.js
+++ slashCommands/info/ping.js
@@ -3,11 +3,14 @@ const { Client, Interaction } = require("discord.js");
  * @param {Client} client
  * @param {Interaction} interaction
  */
-module.exports.run =  (client,interaction) => {
+module.exports.run = (client, interaction) => {
     let d2 = interaction.createdTimestamp
     interaction.reply(`${d2 - Date.now()} ms`)
+    console.log(interaction.options)
 }
 module.exports.conf = {
     "name": "ping",
-    "description": "Calculating bot's ping."
+    "description": ["Bot pingini ölçer.", "Calculates bot's ping."], //first element for turkish and second one for english
+    "options": [],
+    "category": "info"
 }
