3531e448c53f5d37ae0cde21f33994de5601c68e
cFyt <45104771+cFyt@users.noreply.github.com>
2021-09-12 18:31:53 +0300

    Update getLang.js

--- utils/getLang.js
+++ utils/getLang.js
@@ -5,8 +5,14 @@ const en = require('../langs/en.json')
 /**
  * @param {Message} message
  */
-function getLang(message) {
-    let lang = db.get(`lang.${message.guild.id}`)
+function getLang(message, id) {
+    let lang;
+    if(id){
+        lang = db.get(`lang.${id}`)
+    }else{
+        lang = db.get(`lang.${message.guild.id}`)
+    }
+    
     if (lang) {
         if (lang == "tr") {
             flang = tr
