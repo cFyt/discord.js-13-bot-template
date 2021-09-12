ebb83e6c77fb8699051bab2df6fe07fb6e26ca01
cFyt <45104771+cFyt@users.noreply.github.com>
2021-09-12 18:25:21 +0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,12 +1,13 @@
+const { Collection } = require('discord.js')
 const { commandRefresh } = require('../utils/commandRefresh')
 
 module.exports = guild => {
-    console.log("New guild: ", guild.name)
+    console.log("New Server: ", guild.name)
     
     let client = guild.client
     let cmds = []
     client.slash.each(prob => {
-        cmds.push({ name: prob.conf.name, description: prob.conf.description })
+        cmds.push({ name: prob.conf.name, description: prob.conf.description, options: prob.conf.options })
     })
     commandRefresh(guild.id, cmds)
 }
